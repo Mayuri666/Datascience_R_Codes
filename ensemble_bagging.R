e0e5691f43291988b67b9272b222610b82f3e153
Mayuri666 <60732350+Mayuri666@users.noreply.github.com>
2020-03-03 13:57:16 +0530

    Add files via upload

---
+++ ensemble_bagging.R
@@ -0,0 +1,23 @@
+#Bagging, we just have to add a for loop rest data partition remains same
+acc<-c()
+for(i in 1:500)
+{
+  print(i)
+  #data partition
+  inTraininglocal<-createDataPartition(iris$Species,p=.85,list=F)
+  training<-iris[inTraininglocal,]
+  testing<-iris[-inTraininglocal,]
+  
+  #Model building
+  model<-C5.0(training$Species~.,data=training)
+  
+  #generate the model summary
+  summary(model)
+  #predict for test dataset
+  pred<-predict.C5.0(model,testing[,-5])
+  a<-table(testing$Species,pred)
+  #accuracy
+  acc<-c(acc,sum(diag(a))/sum(a))
+}
+summary(acc)
+boxplot(acc)
